ec70d123ba1dd996589847a8e68f0fc62c8589be
Voogie James <voogie@origin.no>
2018-03-19 16:30:57 +0100

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+'use strict';
+
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 9012
 
@@ -6,6 +10,7 @@ app.get('/', (req, res) => {
     res.send('Hello There ...');
 });
 
+app.use(cors());
 app.use(express.static('filehandler'));
 
 app.listen(9012, () => {
