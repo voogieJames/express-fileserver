a962b353771320fae38055943f062c9c6cd5cb23
Voogie James <voogie@origin.no>
2018-03-20 15:21:36 +0100

    updated server

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 'use strict';
 
 const express = require('express');
-const cors = require('cors');
 
-const app = express();
 const port = 9012
-
-app.get('/', (req, res) => {
-    res.send('Hello There ...');
+const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-app.use(cors());
-app.use(express.static('filehandler'));
+app.get('/cta.kml', (req, res, next) => {
+    res.type('application/vnd.google-earth.kml+xml');
+    res.sendFile(__dirname + '/filehandler/cta.kml');
+    console.log(' - cta ok -');
+});
 
 app.listen(9012, () => {
     console.log(' -> server is on');
